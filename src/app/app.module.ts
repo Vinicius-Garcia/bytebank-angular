5ab9c8b98995f5b4d6a8d8e8af8537cc601c2631
Vinicius <viniciusgarcia1300@gmail.com>
2022-04-23 12:04:38 -0300

    Criando projeto do curso de Angular da Alura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './componentes/nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './componentes/extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { AppRoutingModule } from './app-routing.module';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt'}, // muda idioma angular para pt
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' } // muda valor da moede para br
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/componentes/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { Transferencia } from '../../models/transferencia.model'
+import { TransferenciaService } from "../../services/transferencia.service";
+
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+    @Output() aoTransferir = new EventEmitter<any>();
+
+    valor: number;
+    destino: string;
+
+    constructor(
+        private transferenciaService: TransferenciaService,
+        private router: Router
+    ) {}
+    
+    novaTransferencia() {
+        const novaTransferencia: Transferencia = {
+            valor: this.valor,
+            destino: this.destino
+        }
+        this.transferenciaService.adicionar(novaTransferencia).subscribe((resultado) => {
+            console.log(resultado);
+            this.router.navigateByUrl('extrato');
+        },
+        error => console.log(error));
+    }
+}
